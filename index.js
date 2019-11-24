226ea16ddbf80d18f6ba72aafccec2c1f73dc4b4
Neal Makh <nealmakh@hotmail.co.uk>
2019-11-24 01:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
